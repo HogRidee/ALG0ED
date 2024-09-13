d1abbbd0fdaa8da764b301c1e2211631d1084655
HogRidee <iaraoz061102@gmail.com>
2024-09-13 11:52:51 -0500

    Start practice for lab01

---
+++ Laboratorios/01/2024-1/P1/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: ivana
+ *
+ * Created on 13 de septiembre de 2024, 11:46 AM
+ */
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
